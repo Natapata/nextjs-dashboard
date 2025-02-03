e9ee1b4b51985ca8dd71312cf15bddf44e8d4a63
Natapata <83461493+Natapata@users.noreply.github.com>
2025-02-03 17:50:22 +0100

    fix_overviepage_metaData

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -4,6 +4,12 @@ import CardWrapper from '@/app/ui/dashboard/cards';
 import { lusitana } from '@/app/ui/fonts';
 import { Suspense } from 'react';
 import { RevenueChartSkeleton, LatestInvoicesSkeleton, CardsSkeleton } from '@/app/ui/skeletons';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: "Dashboard",
+  description: "View Dashboard",
+};
 
 export default async function Page() {
 
