575d4b8ec90c61b7c6c37bebd0888f3a33db08ab
Natalie Turek <83461493+Natapata@users.noreply.github.com>
2025-02-24 10:38:34 +0100

    adding bindings to getUser

--- auth.ts
+++ auth.ts
@@ -5,17 +5,34 @@ import { z } from 'zod';
 import type { User } from '@/app/lib/definitions';
 import bcrypt from 'bcrypt';
 import postgres from 'postgres';
+import pgPromise from 'pg-promise';
 
-const sql = postgres(process.env.POSTGRES_URL!, {ssl: 'require'} );
+//const sql = postgres(process.env.POSTGRES_URL!, {ssl: 'require'} );
+
+const pgp = pgPromise();
+const db = pgp(process.env.POSTGRES_URL!, {ssl: 'require'});
  
-async function getUser(email: string): Promise<User | undefined> {
-    try{
-        const user = await sql<User[]>`SELECT * FROM users WHERE email = ${email}`;
-        return user[0];
-    }catch(error){
-        console.error('Database Error:', error);
-        throw new Error('Failed to fetch user.');
-    }
+// async function getUser(email: string): Promise<User | undefined> {
+//     try{
+//         const user = await sql<User[]>`SELECT * FROM users WHERE email = ${email}`;
+//         return user[0];
+//     }catch(error){
+//         console.error('Database Error:', error);
+//         throw new Error('Failed to fetch user.');
+//     }
+// }
+
+async function getUserWithBind(email: string): Promise<User | undefined> {
+  try {
+    const user = await db.oneOrNone<User>(
+      'SELECT * FROM users WHERE email = $1',
+      [email]
+    );
+    return user || undefined;
+  } catch (error) {
+    console.error('Database Error:', error);
+    throw new Error('Failed to fetch user.');
+  }
 }
 
 export const { auth, signIn, signOut } = NextAuth({
@@ -29,14 +46,13 @@ export const { auth, signIn, signOut } = NextAuth({
             
             if (parsedCredentials.success) {
                 const { email, password } = parsedCredentials.data;
-                const user = await getUser(email);
+                const user = await getUserWithBind(email);
                 if (!user) return null;
                 const passwordsMatch = await bcrypt.compare(password, user.password);
-       
                 if (passwordsMatch) return user;
               }
        
-              console.log('Invalid credentials');
+              console.log('Invalid email or password.');
               return null;
             },
           }),
